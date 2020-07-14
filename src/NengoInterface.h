d19964f30afb6e01457642194ddbb126a63cf8b7
Mark Plagge <plaggm@rpi.edu>
2020-07-14 15:36:26 -0400

    adding documentation

--- src/NengoInterface.h
+++ src/NengoInterface.h
@@ -8,7 +8,12 @@
 #include <include/pybind11/embed.h>
 namespace py = pybind11;
 namespace neuro_os {
+	/**
+	 * NengoInterface Struct
+	 * Struct that contains the python bindings for the nengo operating system scheduler. On init compiles the nengo network
+	 */
 	struct NengoInterface {
+
 		py::scoped_interpreter guard{};
 		py::module sim_mod = py::module::import("nengo_os");
 		py::object nengo_os_iface;
@@ -25,17 +30,48 @@ namespace neuro_os {
 		std::vector<int> wait_q;
 	};
 
+	/**
+	 * Given an instance of a NengoInterface struct, run the simulation for a set number of ticks.
+	 * @param nengo NengoInterface containing current scheduler algorithm
+	 * @param n_ticks Number of ticks to run
+	 */
 	void run_sim_n_ticks(const NengoInterface& nengo, int n_ticks);
+	/**
+	 * For precomputation of the scheduler stats. This runs the simulation for n_ticks and saves the state changes
+	 * @param nengo
+	 * @param n_ticks Number of ticks to compute. Should be greater than the end of the ROSS simulation
+	 */
 	void run_precompute_sim(const NengoInterface& nengo, int n_ticks);
 	std::vector<int> precompute_run_q(const NengoInterface& nengo);
 	std::vector<int> precompute_wait_q(const NengoInterface& nengo);
 
 	std::vector<sim_proc::SimProcess> convert_py_list(py::list py_list);
+	/**
+	 * The NengoInterface struct, when precomputed, contains a current epoch value. This
+	 * returns the currently running processes (SimProcess objects) in a vector at the current epoch
+	 * @param nengo
+	 * @return
+	 */
 	std::vector<neuro_os::sim_proc::SimProcess> precompute_run_proc(const NengoInterface& nengo);
+	/**
+	 * The NengoInterface struct, when precomputed, contains a current epoch value. This
+	 * returns the currently waiting & pre-wait processes (SimProcess objects) in a vector at the current epoch
+	 * @param nengo
+	 * @return
+	 */
 	std::vector<neuro_os::sim_proc::SimProcess> precompute_wait_proc(const NengoInterface& nengo);
-
+	/**
+	 * Increments the current epoch time for precompute mode.
+	 * @param nengo
+	 */
 	void increment_pc(const NengoInterface& nengo);
+	/**
+	 * returns the current status of the nengo interface -waiting, running, and current epoch-
+	 * @param nengo
+	 * @return
+	 */
 	NengoSchedulerStatus get_nengo_status(const NengoInterface& nengo);
+
 	std::vector<int> convert_q(py::list& pylist);
 }// namespace neuro_os
 
